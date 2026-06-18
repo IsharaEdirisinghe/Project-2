08400ec4ba7da8b5a61d0f485e7b7421c8157995
Ishara <isharaedirisinghe800@gmail.com>
2026-06-18 05:56:44 +0530

    day 21 ✏️

--- controllers/productController.js
+++ controllers/productController.js
@@ -132,3 +132,22 @@ export async function getProductById(req,res){
         })
     }
 }
+
+export async function searchProducts(req,res) {
+    const searchQuery = req.params.query
+    try{
+        const products = await Product.find({
+            $or:[
+                {name: {$regex : searchQuery, $options : "i"}},
+                {altNames : {$eleMatch : {$regex : searchQuery, $options : "i"}}}
+            ],
+            isAvailable : true
+        })
+        res.json(products)
+    }catch(err){
+        res.status(500).json({
+            message : "Internal server error",
+            error : err
+        })
+    }
+}
